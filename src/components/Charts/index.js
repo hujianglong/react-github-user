11ebc237e4f10f1aa33a728263ffbb6b182d5441
jianglong.hu@emapgo.com.cn <kotei357>
2020-12-21 11:26:33 +0800

    init react search github user proj

---
+++ src/components/Charts/index.js
@@ -0,0 +1,7 @@
+import ExampleChart from "./ExampleChart";
+import Column3D from "./Column3D";
+import Bar from "./Bar";
+import Pie from "./Pie";
+import Doughnut2D from "./Doughnut2d";
+
+export { ExampleChart, Pie, Column3D, Bar, Doughnut2D };
